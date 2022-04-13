a195bedaf3334013f67850b853725b22f02611a2
MomanyiPoet <momanyiturner@gmail.com>
2022-04-13 15:12:38 -0700

    My third commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
 /**
  *main - tests function prints if integer positive or negative
  *Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -12,4 +12,5 @@ int main(void)
 	positive_or_negative(i);
 
 	return(0);
+
 }
