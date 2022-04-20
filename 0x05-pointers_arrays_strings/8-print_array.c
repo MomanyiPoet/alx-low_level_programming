7a0c9060ed4b34f3a8e86591084cbbff1ab25a3d
MomanyiPoet <momanyiturner@gmail.com>
2022-04-20 14:51:25 -0700

    My second commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,13 +14,13 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		printf ("%d", a[i]);
+		printf("%d", a[i]);
 
 		if (i < n - 1)
 		{
-			printf (", ");
+			printf(", ");
 		}
 		i++;
 	}
-	printf ("\n");
+	printf("\n");
 }
