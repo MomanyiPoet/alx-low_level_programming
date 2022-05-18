fdffdca0866e76abe535411f9db53e8d72f086be
MomanyiPoet <momanyiturner@gmail.com>
2022-05-18 14:30:13 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -48,10 +48,8 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b != 0)
-	{
 		return (a / b);
-		printf("Error\n"), exit(100);
-	}
+	printf("Error\n"), exit(100);
 }
 
 /**
@@ -64,8 +62,6 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b != 0)
-	{
 		return (a % b);
-		printf("Error\n"), exit(100);
-	}
+	printf("Error\n"), exit(100);
 }
